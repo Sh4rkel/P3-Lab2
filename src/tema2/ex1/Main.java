6c6eb085b73358f88887496f7785fd6117ec9328
Daniel Filip <filipdaniel2000@yahoo.com>
2024-10-12 22:42:11 +0300

    tema

--- src/tema2/ex1/Main.java
+++ src/tema2/ex1/Main.java
@@ -1,6 +1,7 @@
 package tema2.ex1;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     /**
@@ -17,6 +18,19 @@ public class Main {
         MagazinMobilier magazin2 = new MagazinMobilier("Dedeman", "Strada Independentei", Arrays.asList(mobila3));
 
         MagazinMobilier[] magazine = {magazin1, magazin2};
-        System.out.println(MagazinMobilier.magazinCuCeleMaiIeftineMobile(magazine));
+
+        // Gasim magazinul cu cele mai ieftine mobile
+        String numeMagazinIeftin = MagazinMobilier.magazinCuCeleMaiIeftineMobile(magazine);
+        System.out.println("Magazinul cu cele mai ieftine obiecte de mobilier este: " + numeMagazinIeftin);
+
+        // Definim o lista de cuvinte pentru cautare în descrierile mobilelor
+        List<String> cuvinteCautare = Arrays.asList("lemn", "sticla");
+
+        // Cautam cate mobile din fiecare magazin conțin toate cuvintele din lista de cautare
+        for (MagazinMobilier magazin : magazine) {
+            int numarMobile = magazin.numaraMobileCuDescriere(cuvinteCautare);
+            System.out.println("Magazinul " + magazin.getNume() + " are " + numarMobile +
+                    " obiecte de mobilier care contin toate cuvintele 'lemn' si 'sticla' în descriere.");
+        }
     }
 }
